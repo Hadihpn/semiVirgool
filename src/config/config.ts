458a83d57f398d115c4552367a6e78d4e58d126c
persnb <persnb@gmail.com>
2025-06-08 19:17:32 +0330

    add config file

---
+++ src/config/config.ts
@@ -0,0 +1,23 @@
+import { registerAs } from "@nestjs/config";
+
+export enum ConfigKeys {
+  App = "App",
+  Db = "Db",
+  Jwt="Jwt"
+}
+
+const AppConfig = registerAs(ConfigKeys.App, () => ({
+  port: 3000,
+}));
+const DbConfig = registerAs(ConfigKeys.Db, () => ({
+  port: 5433,
+  host: "localhost",
+  username: "postgres",
+  password: "HADIh3pn",
+  database: "semiVirgool",
+}));
+const JwtConfig= registerAs(ConfigKeys.Jwt,()=>({
+  accessTokenSecret:"4d49ce5838a508fe14d76f4e6a8045fd",
+  refreshTokenSecret:"0ac03851e944231e8b05e553dc6637f8"
+}))
+export const configurations = [AppConfig, DbConfig,JwtConfig];
