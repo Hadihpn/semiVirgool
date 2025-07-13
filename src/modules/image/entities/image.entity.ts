030f9919b1d0bd81fb8f6d85cbc3bc6cee953cfe
persnb <persnb@gmail.com>
2025-07-14 00:40:17 +0330

    add @afterLoad for change image location

--- src/modules/image/entities/image.entity.ts
+++ src/modules/image/entities/image.entity.ts
@@ -1,7 +1,7 @@
 import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntityEnum } from "src/common/enum/entity.enum";
 import { UserEntity } from "src/modules/user/entities/user.entity";
-import { Column, Entity, ManyToOne } from "typeorm";
+import { AfterLoad, Column, Entity, ManyToOne } from "typeorm";
 @Entity(EntityEnum.Image)
 export class ImageEntity extends BaseEntity {
     @Column()
@@ -14,5 +14,9 @@ export class ImageEntity extends BaseEntity {
     userId:number
     @ManyToOne(()=>UserEntity,user=>user.images,{onDelete:"CASCADE"})
     user:UserEntity
+    @AfterLoad()
+    map(){
+        this.location = `http://localhost:3000/${this.location}`
+    }
 
 }
